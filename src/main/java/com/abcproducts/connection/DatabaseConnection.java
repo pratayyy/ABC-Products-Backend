817d285e2f873e9e58de7c659afee41e404c1adf
pratayyy <pratayroy_official@yahoo.com>
2024-03-14 23:01:15 +0530

    Method to connect to database added

--- src/main/java/com/abcproducts/connection/DatabaseConnection.java
+++ src/main/java/com/abcproducts/connection/DatabaseConnection.java
@@ -1,5 +1,22 @@
 package com.abcproducts.connection;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DatabaseConnection {
+	private static String URL = "jdbc:mysql://localhost:3306/hrc";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "kiit1234";
+	
+	private Connection connection = null;
+	
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
 }
